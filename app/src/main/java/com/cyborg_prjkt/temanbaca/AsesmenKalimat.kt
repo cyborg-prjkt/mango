85820e39afbd066148ee4e946b8ff2ef90ef676d
cyborg <cyborgprjkt@gmail.com>
2025-11-25 07:26:01 +0700

    update

---
+++ app/src/main/java/com/cyborg_prjkt/temanbaca/AsesmenKalimat.kt
@@ -0,0 +1,20 @@
+package com.cyborg_prjkt.temanbaca
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.widget.Button
+import android.widget.TextView // Import TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Random
+
+@Suppress ("DEPRECATION")
+class AsesmenSukuKata : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.asesmen_kalimat)
+    }
+}
