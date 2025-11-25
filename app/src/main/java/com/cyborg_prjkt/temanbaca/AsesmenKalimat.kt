c0ea35a84c58ce602ff133ed34504ad9217765eb
cyborg <cyborgprjkt@gmail.com>
2025-11-25 11:13:25 +0700

    update

--- app/src/main/java/com/cyborg_prjkt/temanbaca/AsesmenKalimat.kt
+++ app/src/main/java/com/cyborg_prjkt/temanbaca/AsesmenKalimat.kt
@@ -11,10 +11,51 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
 
 @Suppress ("DEPRECATION")
-class AsesmenSukuKata : AppCompatActivity() {
+class AsesmenKalimat : AppCompatActivity() {
+
+    private val daftarKalimat: List<String> = listOf(
+        "Ibu sedang memasak nasi di dapur.",
+        "Adi pergi ke sekolah dengan memakai sepatu baru.",
+        "Kucing itu tidur di atas bantal yang empuk.",
+        "Langit terlihat sangat biru dan cerah hari ini.",
+        "Saya suka membaca buku cerita fantasi.",
+        "Tolong tutup pintu itu dengan pelan.",
+        "Pohon mangga di depan rumah sudah berbuah.",
+        "Ayah membeli koran di warung dekat rumah."
+    )
+
+    private lateinit var btnNext: Button
+    private lateinit var tvKalimatList: List<TextView>
+    private val randomGenerator = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.asesmen_kalimat)
+
+        btnNext = findViewById(R.id.next)
+        val tvlist = listOf(
+            R.id.kalimat_1,
+            R.id.kalimat_2,
+            R.id.kalimat_3,
+            R.id.kalimat_4,
+            R.id.kalimat_5,
+        )
+        tvKalimatList = tvlist.map { findViewById(it) }
+        KalimatAcak()
+
+        btnNext.setOnClickListener {
+            KalimatAcak()
+        }
+    }
+    private fun KalimatAcak() {
+        val uniqueIndices = mutableSetOf<Int>()
+        while (uniqueIndices.size < 5) {
+            val randomIndex = randomGenerator.nextInt(daftarKalimat.size)
+            uniqueIndices.add(randomIndex)
+        }
+        val kalimatAcakList = uniqueIndices.map { daftarKalimat[it] }
+        tvKalimatList.forEachIndexed { index, textView ->
+            textView.text = kalimatAcakList[index]
+        }
     }
 }
